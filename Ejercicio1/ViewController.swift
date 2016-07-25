d0ad1d6ac114590939d8d69adbeb07c674f23eb7
Héctor Cuevas Morfín <poncho@Hectors-MacBook-Pro-2.local>
2016-07-25 12:48:32 -0500

    xcode project added

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by Héctor Cuevas Morfín on 7/25/16.
+//  Copyright © 2016 AppDataMX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
